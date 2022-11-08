736b1ab7bb5ea2d9e69ec27af95263f1b93b804b
Robin <robinpinondev@gmail.com>
2022-11-08 14:03:33 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+// =============================================
+// Scene
+// =============================================
+
+const scene = new THREE.Scene()
+
+// =============================================
+// Red cube
+// =============================================
+
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({color: 0xff0000})
+const mesh = new THREE.Mesh(geometry, material)
+
+scene.add(mesh)
+
+// =============================================
+// Camera
+// =============================================
+
+const camera = new THREE.PerspectiveCamera()
+scene.add(camera)
